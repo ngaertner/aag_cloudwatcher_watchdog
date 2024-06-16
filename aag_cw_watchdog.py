8b7786b4b4c77cc6185d7f2e0c88c6d0e9b0ff12
ngaertner <n.gaertner@gmx.net>
2024-06-16 14:42:38 +0200

    format fixes

--- aag_cw_watchdog.py
+++ aag_cw_watchdog.py
@@ -161,10 +161,10 @@ if os.path.isfile(cloudwather_exe_file) == False:
 if os.path.isfile(aag_json_file):
     st=os.stat(aag_json_file)
     mtime=st.st_mtime
-    last_change = datetime.fromtimestamp(mtime).strftime("%Y-%m-%d, %H:%M:%S")
+    last_change = datetime.fromtimestamp(mtime)
     
     json_age = int((datetime.now() -last_change).total_seconds())
-    logger.info(f"{CLOUD_WATCHER_AAG_JSON} - last change = {last_change} - age is {json_age} seconds")
+    logger.info(f"{CLOUD_WATCHER_AAG_JSON} - last change = {last_change.strftime("%Y-%m-%d, %H:%M:%S")} - age is {json_age} seconds")
     if json_age > CLOUD_WATCHER_TIMEOUT_SEC:
         logger.warning(f"{CLOUD_WATCHER_AAG_JSON} file is not up to date!")
 else:
