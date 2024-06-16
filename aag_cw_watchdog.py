310b9920f0b22b464f4196d0a8283d5dc32b72a6
ngaertner <n.gaertner@gmx.net>
2024-06-16 14:33:13 +0200

    copyright and disclaimer

--- aag_cw_watchdog.py
+++ aag_cw_watchdog.py
@@ -1,3 +1,32 @@
+#BSD 3-Clause License
+#
+#Copyright (c) 2024, Nico Gärtner
+#
+#Redistribution and use in source and binary forms, with or without
+#modification, are permitted provided that the following conditions are met:
+#
+#1. Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+#2. Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+#3. Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+#FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import os
 import time
 from datetime import datetime
@@ -10,6 +39,7 @@ import pathlib
 import argparse
 import winreg
 
+SCRIPT_VERSION = 'v0.0.2'
 
 ############## BEGIN - SET YOUR CONSTANTS HERE - BEGIN ######################
 
@@ -97,11 +127,26 @@ cloudwather_exe_file = pathlib.Path(CLOUD_WATCHER_PATH, CLOUD_WATCHER_EXE)
 aag_json_file = pathlib.Path(CLOUD_WATCHER_OUTPUT_PATH, CLOUD_WATCHER_AAG_JSON)
 
 print("")
-print("###################### ENVIRONMENT INFO #########################")
+print("####################### AAG Cloud Watcher Watchdog ####################")
+print("")
+print(f"VERSION   {SCRIPT_VERSION}")
+print("AUTHOR    Nico Gärtner - https://github.com/ngaertner")
+print("LICENSE   BSD 3-Clause License")
+print("")
+print("############################## DISCLAIMER #############################")
+print('THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,')
+print('EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF')
+print('MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND')
+print('NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE')
+print('LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION')
+print('OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION')
+print('WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.')
+print("")
+print("######################### ENVIRONMENT INFO ############################")
 print(f"Cloud Watcher: {cloudwather_exe_file}")
 print(f"File to check: {aag_json_file}")
 print(f"Log File:      {log_file}")
-print("#################################################################")
+print("#######################################################################")
 print("")
 
 #### checks
@@ -117,14 +162,14 @@ if os.path.isfile(aag_json_file):
     st=os.stat(aag_json_file)
     mtime=st.st_mtime
     last_change = datetime.fromtimestamp(mtime)
-    json_age = (datetime.now() -last_change).total_seconds()
+    json_age = int((datetime.now() -last_change).total_seconds())
     logger.info(f"{CLOUD_WATCHER_AAG_JSON} - last change = {last_change} - age is {json_age} seconds")
     if json_age > CLOUD_WATCHER_TIMEOUT_SEC:
         logger.warning(f"{CLOUD_WATCHER_AAG_JSON} file is not up to date!")
 else:
     logger.warning(f'{aag_json_file} does not exist!')
     last_change = datetime.min
-    json_age = (datetime.now() -last_change).total_seconds()
+    json_age = int((datetime.now() -last_change).total_seconds())
 
 #### restart logic
 
