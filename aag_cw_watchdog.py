6b2c58ec898aa2b27e20042120deeeee56c419ec
Nico Gärtner <47719834+ngaertner@users.noreply.github.com>
2024-06-16 13:28:55 +0200

    improvements in file structure

--- aag_cw_watchdog.py
+++ aag_cw_watchdog.py
@@ -7,10 +7,7 @@ import psutil
 import logging
 import logging.handlers
 import pathlib
-from io import StringIO
 import argparse
-import tempfile
-
 
 ############## BEGIN - SET YOUR CONSTANTS HERE - BEGIN ######################
 
@@ -19,24 +16,30 @@ import tempfile
 CLOUD_WATCHER_EXE         = "AAG_CloudWatcher.exe"
 CLOUD_WATCHER_PATH        = "C:/Program Files (x86)/AAG_CloudWatcher"
 CLOUD_WATCHER_TIMEOUT_SEC = 120
-LOG_FILE                  = "aag_watchdog.log"
+CLOUD_WATCHER_AAG_JSON    = "aag_json.dat"
+CLOUD_WATCHER_OUTPUT_PATH = ""
+LOG_DIR                   = ""
+LOG_FILE                  = "aag_cw_watchdog_log"
 LOG_LEVEL                 = logging.NOTSET
 
 ############## END - SET YOUR CONSTANTS HERE - END ######################
 
+
+#### initialize argparser for CLI
 parser = argparse.ArgumentParser()
-parser.add_argument('--cw_path', help='Cloud Watcher Path (contains AAG_CloudWatcher.exe)', default=CLOUD_WATCHER_PATH)
-parser.add_argument('--out_path', help='Cloud Watcher Output Path (contanis aag_json.dat)',required=True)
-parser.add_argument('--timeout', help='Timeout to restart inactive Cloud Watcher in seconds (default 120s)', default=CLOUD_WATCHER_TIMEOUT_SEC)
-parser.add_argument('--log_file', help='Logfile', default=LOG_FILE)
-parser.add_argument('--log_level', help='Log Level', default='INFO', choices=['INFO','WARNING','ERROR'])
+parser.add_argument('--cw_path', help="Cloud Watcher Path (contains AAG_CloudWatcher.exe)", default=CLOUD_WATCHER_PATH)
+parser.add_argument('--out_path', help=f"Cloud Watcher Output Path (contains {CLOUD_WATCHER_AAG_JSON})", default=CLOUD_WATCHER_OUTPUT_PATH)
+parser.add_argument('--timeout', help=f"Timeout to restart inactive Cloud Watcher in seconds - default {CLOUD_WATCHER_TIMEOUT_SEC}s)", default=CLOUD_WATCHER_TIMEOUT_SEC)
+parser.add_argument('--log_dir', help="Directory to store Log Files", default=LOG_DIR)
+parser.add_argument('--log_file', help=f"Log Filename - default {LOG_FILE}", default=LOG_FILE)
+parser.add_argument('--log_level', help="Log Level (INFO|WARNING|ERROR) - default INFO", default='INFO', choices=['INFO','WARNING','ERROR'])
 
 args = parser.parse_args()
 
 CLOUD_WATCHER_PATH = args.cw_path
 CLOUD_WATCHER_OUTPUT_PATH = args.out_path
 CLOUD_WATCHER_TIMEOUT_SEC = args.timeout
-CLOUD_WATCHER_LOG_FILE = args.log_file
+LOG_DIR = args.log_dir
 
 match args.log_level:
     case 'INFO':
@@ -48,47 +51,70 @@ match args.log_level:
     case _:
         LOG_LEVEL = LOG_LEVEL
 
-#logging.basicConfig(encoding='utf-8')
+#### build filenames to use in the script
+
+cloudwather_exe_file = pathlib.Path(CLOUD_WATCHER_PATH, CLOUD_WATCHER_EXE)
+aag_json_file = pathlib.Path(CLOUD_WATCHER_OUTPUT_PATH, CLOUD_WATCHER_AAG_JSON)
+if LOG_DIR == "":
+    log_file = pathlib.Path(LOG_DIR, LOG_FILE)
+else:
+    log_file = pathlib.Path(os.getcwd(), LOG_FILE)
+
+print("")
+print("###################### ENVIRONMENT INFO #########################")
+print(f"Cloud Watcher: {cloudwather_exe_file}")
+print(f"File to check: {aag_json_file}")
+print(f"Log File:      {log_file}")
+print("#################################################################")
+print("")
+
+#### logger initialization
+
+
 logger = logging.getLogger('AAG_CW_WATCHDOG')
 
 log_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S")
 
-log_file = logging.handlers.TimedRotatingFileHandler(LOG_FILE, 'D', 1, 5)
-log_file.setLevel(LOG_LEVEL)
-log_file.setFormatter(log_formatter)
+log_file_handler = logging.handlers.TimedRotatingFileHandler(log_file, 'midnight', 1, 7)
+log_file_handler.suffix = "%Y-%m-%d"
+log_file_handler.setLevel(LOG_LEVEL)
+log_file_handler.setFormatter(log_formatter)
 
 log_handler = logging.StreamHandler(sys.stdout)
 log_handler.setLevel(LOG_LEVEL)
 log_handler.setFormatter(log_formatter)
 
 logger.addHandler(log_handler)
-logger.addHandler(log_file)
+logger.addHandler(log_file_handler)
 logger.setLevel(LOG_LEVEL)
 
-cloudwather_exe_file = pathlib.Path(CLOUD_WATCHER_PATH, CLOUD_WATCHER_EXE)
-aag_json_file = pathlib.Path(CLOUD_WATCHER_OUTPUT_PATH, "aag_json.dat")
-
+#### checks
 
 # check if cloud watcher exe exists
 if os.path.isfile(cloudwather_exe_file) == False:
     logger.error(f'Cloud Watcher EXE File "{cloudwather_exe_file} was not found!')
-    log_file.flush()
+    log_file_handler.flush()
     sys.exit()
-
 
 # check if aag_json.dat exists already
 if os.path.isfile(aag_json_file):
     st=os.stat(aag_json_file)
     mtime=st.st_mtime
     last_change = datetime.fromtimestamp(mtime)
-    logger.info(f"{aag_json_file} last change = {last_change}")
+    json_age = (datetime.now() -last_change).total_seconds()
+    logger.info(f"{CLOUD_WATCHER_AAG_JSON} - last change = {last_change} - age is {json_age} seconds")
+    if json_age > CLOUD_WATCHER_TIMEOUT_SEC:
+        logger.warning(f"{CLOUD_WATCHER_AAG_JSON} file is not up to date!")
 else:
     logger.warning(f'{aag_json_file} does not exist!')
     last_change = datetime.min
+    json_age = (datetime.now() -last_change).total_seconds()
 
+#### restart logic
 
-if ( (datetime.now() -last_change).total_seconds() > CLOUD_WATCHER_TIMEOUT_SEC ):
+if (json_age > CLOUD_WATCHER_TIMEOUT_SEC ):
     # trigger restart of cloudwatcher
+    logger.info("Initiating (re-)start of Cloud Watcher.")
 
     if (CLOUD_WATCHER_EXE not in (p.name() for p in psutil.process_iter())):
        logger.warning("Cloud Watcher is not running!")
@@ -96,12 +122,12 @@ if ( (datetime.now() -last_change).total_seconds() > CLOUD_WATCHER_TIMEOUT_SEC )
        logger.info("Killing running Cloud Watcher!")
        with open(os.devnull, 'w') as fp:
           sp = subprocess.Popen("taskkill /f /im " + CLOUD_WATCHER_EXE, shell=True, stdout = fp, stderr=subprocess.STDOUT)
-       time.sleep(1)
+       time.sleep(2) #wait for two seconds to kill process
        if (CLOUD_WATCHER_EXE not in (p.name() for p in psutil.process_iter())):
             logger.info("Cloud Watcher successfully killed!")
        else:    
-           logger.error("Cloud Watcher could not be killed!")
-           log_file.flush()
+           logger.critical("Cloud Watcher could not be killed!")
+           log_file_handler.flush()
            sys.exit()
 
     logger.info("Starting Cloud Watcher!")
@@ -110,4 +136,4 @@ if ( (datetime.now() -last_change).total_seconds() > CLOUD_WATCHER_TIMEOUT_SEC )
     if (CLOUD_WATCHER_EXE not in (p.name() for p in psutil.process_iter())):
         logger.error("Cloud Watcher could not be started!")
 
-    log_file.flush()
+    log_file_handler.flush()
